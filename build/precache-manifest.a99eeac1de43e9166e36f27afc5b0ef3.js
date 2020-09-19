8ca69a20a34f9b58b66290ae280b39307fd80f0d
Huiyu <iownthegame@gmail.com>
2020-09-19 10:24:28 +0200

    uibuild

---
+++ build/precache-manifest.a99eeac1de43e9166e36f27afc5b0ef3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2176c3db34963e6ff2fe512fe62c9b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1da6b66b73f4474d6358",
+    "url": "/static/css/main.f71eae43.chunk.css"
+  },
+  {
+    "revision": "fdc00f3d2f4001d77518",
+    "url": "/static/js/2.9d147208.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9d147208.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1da6b66b73f4474d6358",
+    "url": "/static/js/main.74dc2395.chunk.js"
+  },
+  {
+    "revision": "9d2b4f7262278411ffe7",
+    "url": "/static/js/runtime-main.4464e187.js"
+  }
+]);
