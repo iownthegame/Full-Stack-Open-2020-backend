8a24b3737b6fe71e71f6bd836987dbfa656a0d06
Huiyu <iownthegame@gmail.com>
2020-09-19 16:15:04 +0200

    ex 3.19 - add mongoose validation

--- models/person.js
+++ models/person.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose')
+mongoose.set('useCreateIndex', true)
 
+const uniqueValidator = require('mongoose-unique-validator')
 const url = process.env.MONGODB_URI
 
 console.log('connect to ', url)
@@ -13,9 +15,18 @@ mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
   })
 
   const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
+    name: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    number: {
+      type: String,
+      required: true
+    }
   })
+
+  personSchema.plugin(uniqueValidator)
 
   personSchema.set('toJSON', {
     transform: (document, returnedObject) => {
